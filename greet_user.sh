5511cc07da0c18927b4815bb88bdeb67fe746cd9
Rupam Karmakar <86157946+hackeR200364@users.noreply.github.com>
2023-12-04 20:30:31 +0530

    Create greet_user.sh

---
+++ greet_user.sh
@@ -0,0 +1,11 @@
+# Prompt the user to enter their name
+echo -n "Please enter your name: "
+read userName
+
+# Greet the user
+echo "Hello, $userName! Nice to meet you."
+
+commands
+chmod +x greet_user.sh
+
+./greet_user.sh
